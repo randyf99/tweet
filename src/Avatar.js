59c3f5627ca758dd3bd0c63cf40125e135b16c73
Randy Ferrer <randfer@gmail.com>
2019-02-26 18:09:04 -0500

    Commit static version of tweet component

---
+++ src/Avatar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Avatar() {
+  return (<img src="https://www.gravatar.com/avatar/nothing" className="avatar" alt="avatar" />);
+}
+
+export default Avatar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Avatar from './Avatar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Message() {
+  return (
+    <div className="message">
+      This is a message in a tweet.
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function NameWithHandle() {
+  return (
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+
+const Time = () => (
+  <span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button" />
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle />
+        <Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+ReactDOM.render(<Tweet />, document.getElementById('root'));
+
+
